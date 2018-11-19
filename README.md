6e73e9963c686a75f8e856deddd32cc526c9944c
alan <alan.so@sky.uk>
2018-11-19 22:44:55 +0000

    Add pagination

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Installing to the pyenv 'tools3'
+# Ebay prices
+Search on [ebay](ebay.co.uk) auctions for sold prices and available auctions for sniping on [gixen](gixen.com)
+
+### Example:
+
+```
+$ eprices amazon echo show
+*£60.00 + £0.00    2018-12-04 Tuesday 264023436404 http://www.ebay.co.uk/itm/Amazon-Echo-Show-Smart-Assistant-Black-Locked-Activation-/264023436404
+ £70.00 + £0.00    2018-11-25 Sunday 183542145782 http://www.ebay.co.uk/itm/Amazon-Echo-Connect-Making-Receiving-Phone-Calls-Your-Echo-Dot-Echo-Show-/183542145782
+*£95.00 + £3.45    2018-12-19 Wednesday 183543666908 http://www.ebay.co.uk/itm/Amazon-Echo-Show-Smart-Assistant-Black-/183543666908
+...
+Recently Sold:
+*£110.00 + £6.95 - Monday 2018-11-19 253988429725 http://www.ebay.co.uk/itm/Amazon-Echo-Show-Smart-Assistant-Black-NEW-Seals-intact-/253988429725
+ £102.01 + £3.95 - Sunday 2018-11-18 123496858412 http://www.ebay.co.uk/itm/Amazon-Echo-Show-Smart-Assistant-Black-Mint-Condition-Original-Box-unmark-/123496858412
+...
+
+Available Auctions:
+ £67.00 + £3.45 - Wednesday 2018-11-21 143018389819 http://www.ebay.co.uk/itm/Amazon-Echo-Show-Smart-Assistant-Black-/143018389819
+*£117.00 + £9.00 - Wednesday 2018-11-21 113373025063 http://www.ebay.co.uk/itm/Amazon-Echo-Show-Smart-Assistant-Black-NO-RESERVE-/113373025063
+...
+```
+
+## Installing to the pyenv 'tools3'
 
 **Installation**
 
@@ -15,4 +37,13 @@ sudo cp ebay.yaml /etc
 pyenv activate tools3
 pip uninstall eprices
 pyenv deactivate
+```
+
+**Development**
+
+```
+pyenv virtualenv 3.6.0 ebay
+pyenv local ebay
+pip install -r requirements.txt
+sudo cp ebay.yaml /etc
 ```
--- prices.py
+++ prices.py
@@ -2,6 +2,7 @@
 import argparse
 import datetime
 from collections import Counter
+from pprint import pprint
 
 from ebaysdk.exception import ConnectionError
 from ebaysdk.finding import Connection as Finding
@@ -30,8 +31,19 @@ def find(verb, api_request, product_id):
     try:
         api = Finding(debug=False, domain='svcs.ebay.com', siteid='EBAY-GB', config_file='ebay.yaml')
         response = api.execute(verb, api_request)
+        # pprint(response.dict())
         results = AsJson(response.dict())
+        page = int(results.get_string('paginationOutput.pageNumber'))
+        pages = int(results.get_string('paginationOutput.totalPages'))
         items = results.get_string('searchResult.item')
+
+        while page < pages and page < 4:
+            api_request['paginationInput']['pageNumber'] = page + 1
+            response = api.execute(verb, api_request)
+            # pprint(response.dict())
+            results = AsJson(response.dict())
+            page = int(results.get_string('paginationOutput.pageNumber'))
+            items.extend(results.get_string('searchResult.item'))
 
         if not product_id:
             # Find the most common product_id
@@ -46,15 +58,18 @@ def find(verb, api_request, product_id):
                     product_id = products.most_common(1)[0][0]
 
         for item in items:
-            if AsJson(item).get_string('productId.value') == product_id or product_id is None:
-                # Filter on product id if there is one
-                ending_date = AsJson(item).get_string('listingInfo.endTime')[:10]
-                day = datetime.datetime.strptime(ending_date, '%Y-%m-%d').strftime('%A')
-                amount = float(AsJson(item).get_string('sellingStatus.currentPrice.value'))
-                shipping = float(AsJson(item).get_string('shippingInfo.shippingServiceCost.value'))
-                item_id = AsJson(item).get_string('itemId')
-                url = AsJson(item).get_string('viewItemURL')
-                print('£{:03.2f} + £{:03.2f} - {} {} {} {}'.format(amount, shipping, day, ending_date, item_id, url))
+            ending_date = AsJson(item).get_string('listingInfo.endTime')[:10]
+            day = datetime.datetime.strptime(ending_date, '%Y-%m-%d').strftime('%A')
+            amount = float(AsJson(item).get_string('sellingStatus.currentPrice.value'))
+            shipping = float(AsJson(item).get_string('shippingInfo.shippingServiceCost.value'))
+            item_id = AsJson(item).get_string('itemId')
+            url = AsJson(item).get_string('viewItemURL')
+            total = '£{:03.2f} + £{:03.2f}'.format(amount, shipping)
+            if AsJson(item).get_string('productId.value') == product_id:
+                listing = '*{:<17} {} {} {} {}'.format(total, ending_date, day, item_id, url)
+            else:
+                listing = ' {:<17} {} {} {} {}'.format(total, ending_date, day, item_id, url)
+            print(listing)
         else:
             print()
 
@@ -67,13 +82,33 @@ def find(verb, api_request, product_id):
 
 def sold(search, product_id=''):
     print('Recently Sold:')
-    api_request = {'keywords': search, 'itemFilter': [{'name': 'ListingType', 'value': 'Auction'}, {'name': 'LocatedIn', 'value': 'GB'}, {'name': 'SoldItemsOnly', 'value': True}]}
+    api_request = {'keywords': search,
+                   'sortOrder': 'PricePlusShippingLowest',
+                   'paginationInput': {'pageNumber': 1},
+                   'itemFilter': [{'name': 'ListingType', 'value': 'Auction'},
+                                  {'name': 'LocatedIn', 'value': 'GB'},
+                                  {'name': 'SoldItemsOnly', 'value': True}]}
     return find('findCompletedItems', api_request, product_id)
+
+
+def buy_it_now(search, product_id=''):
+    print('Available as buy it now:')
+    api_request = {'keywords': search,
+                   'itemFilter': [{'name': 'ListingType', 'value': 'FixedPrice'}],
+                   'sortOrder': 'PricePlusShippingLowest',
+                   'paginationInput': {'pageNumber': 1}}
+    # api_request = {'keywords': search, 'itemFilter': [{'name': 'ListingType', 'value': 'AuctionWithBIN'}]}
+    # api_request = {'keywords': search}
+    return find('findItemsAdvanced', api_request, product_id)
 
 
 def available(search, product_id=''):
     print('Available Auctions:')
-    api_request = {'keywords': search, 'itemFilter': [{'name': 'ListingType', 'value': 'Auction'}, {'name': 'LocatedIn', 'value': 'GB'}], 'sortOrder': 'EndTimeSoonest'}
+    api_request = {'keywords': search,
+                   'itemFilter': [{'name': 'ListingType', 'value': 'Auction'},
+                                  {'name': 'LocatedIn', 'value': 'GB'}],
+                   'sortOrder': 'EndTimeSoonest',
+                   'paginationInput': {'pageNumber': 1}}
     return find('findItemsAdvanced', api_request, product_id)
 
 
@@ -94,12 +129,8 @@ def command_line_runner():
     # search = 'Wii Mario Kart'
     # id = '219603758'
     # id = ''
-    # sold(search='ps3 portal 2')
-    # available(search='ps3 portal 2')
-    # sold(search='amazon echo show', product_id='242648070')
-    # find(search='amazon echo show', product_id='242648070')
 
-    # print('Looking for: {}'.format(search))
+    buy_it_now(search, id)
     id = sold(search, id)
     available(search, id)
 
